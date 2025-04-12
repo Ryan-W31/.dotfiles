fddfe8a7ad5e639cecb895298d7429dead6c9a76
Ryan Weisman <ryan.weisman.512@gmail.com>
2025-04-12 15:24:12 -0500

    updated sketchybar

--- sketchybar/.config/sketchybar/items/spaces.sh
+++ sketchybar/.config/sketchybar/items/spaces.sh
@@ -4,31 +4,22 @@
 sketchybar --add event aerospace_workspace_change
 
 # Define your spaces with names and corresponding Nerd Font icons
-#SPACES=("Home:H:Home" "Safari:S:Safari" "Ghostty:T:Terminal" "IDEA:I:IntelliJ IDEA" "Slack:S:Slack" "Mail:M:Mail" "Calendar:C:Calendar" "Music:M:Music")
-SPACES=("Home:H:Home" "Arc:A:Arc" "Ghostty:T:Terminal" "VSCode:V:VSCode" "Music:M:Music")
-
+SPACES=("1" "2" "3" "4" "5" "6" "7" "8" "9")
 # Add and configure spaces
-for SPACE in "${SPACES[@]}"; do
-  WORKSPACE_NAME=${SPACE%%:*} # Extract name (everything before ':')
-  ICON=${SPACE##*:}           # Extract icon (everything after ':')
-  # Everything before the first colon
-
-
-  sketchybar --add item "workspace.$WORKSPACE_NAME" left \
-    --subscribe "workspace.$WORKSPACE_NAME" aerospace_workspace_change \
-    --set "workspace.$WORKSPACE_NAME" \
-    background.padding_left=8 \
-    background.padding_right=8 \
-    icon.font="sketchybar-app-font:Regular:16.0" \
-    icon.padding_left=12 \
-    icon.padding_right=12 \
-    label.drawing=off \
-    label.padding_left=0 \
-    label.padding_right=0 \
-    script="~/.config/sketchybar/plugins/front_app.sh" \
-    icon="$(
-      ~/.config/sketchybar/plugins/icon_map_fn.sh "$ICON"
-    )" \
-    click_script="aerospace workspace $WORKSPACE_NAME" \
-    script="~/.config/sketchybar/plugins/aerospace.sh $WORKSPACE_NAME"
+for SPACE in "${SPACES[@]}";
+    do
+        sketchybar --add item "workspace.$SPACE" left                   \
+            --subscribe "workspace.$SPACE" aerospace_workspace_change   \
+            --set "workspace.$SPACE"                                    \
+            script="~/.config/sketchybar/plugins/aerospace.sh $SPACE"   \
+            icon.font="Zed Mono:Bold:16.0" 		                        \
+            icon="$SPACE"                                               \
+            icon.padding_right=12                                       \
+            icon.width=30                                               \
+            icon.y_offset=1                                             \
+            label.drawing=off                                           \
+            background.drawing=off                                      \
+            click_script="aerospace workspace $SPACE"
 done
+
+sketchybar --add bracket spaces '/workspace\..*/'
--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set "$NAME" \
-    background.color=0xFFC0CAF5 \
-    icon.color=0xFF1A1B26
+  sketchybar --set "$NAME"  \
+    icon.color=0xFFFFC66D   \
+    icon.font="Zed Mono:Bold:16.0"
 else
-  sketchybar --set "$NAME" \
-    icon.color=0xFFC0CAF5 \
-    background.color=0xFF1A1B26
+  sketchybar --set "$NAME"  \
+    icon.color=0xFFC0CAF5   \
+    icon.font="Zed Mono:Normal:16.0"
 fi
